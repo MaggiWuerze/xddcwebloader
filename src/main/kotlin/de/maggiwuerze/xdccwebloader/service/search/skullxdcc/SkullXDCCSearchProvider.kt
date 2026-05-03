b519ca2d0e3016cd15ac8e2cee81b918e1597a0e
MaggiWuerze <dev@maggiwuerze.de>
2026-05-03 22:18:02 +0200

    reenable skull xdcc search

--- src/main/kotlin/de/maggiwuerze/xdccwebloader/service/search/skullxdcc/SkullXDCCSearchProvider.kt
+++ src/main/kotlin/de/maggiwuerze/xdccwebloader/service/search/skullxdcc/SkullXDCCSearchProvider.kt
@@ -1,51 +1,55 @@
-//package de.maggiwuerze.xdccwebloader.service.search.skullxdcc
-//
-//import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-//import de.maggiwuerze.xdccwebloader.model.search.SearchResultItem
-//import de.maggiwuerze.xdccwebloader.service.search.base.SearchClient
-//import de.maggiwuerze.xdccwebloader.service.search.base.SearchEngineTO
-//import de.maggiwuerze.xdccwebloader.service.search.base.SearchProvider
-//import org.springframework.data.domain.Pageable
-//import org.springframework.stereotype.Component
-//
-//@Component
-//class SkullXDCCSearchProvider(
-//    override val searchClient: SearchClient,
-//    override val name: String = "Skull XDCC"
-//) : SearchProvider {
-//
-//    var url: String = "https://skullxdcc.com/ws.php?sterm=%s&"
-//
-//    override fun search(searchTerm: String, pageable: Pageable): List<SearchResultItem> {
-//
-//        searchClient.searchRaw(
-//            baseUrlTemplate = url,
-//            searchTerm = searchTerm,
-//            limitResults = pageable.pageSize,
-//            page = pageable.pageNumber
-//        ).let { result ->
-//
-//            jacksonObjectMapper().readValue(result, Model.SearchResponse::class.java).let { result ->
-//                return result.data.map {
-//                    SearchResultItem(
-//                        server = it.network,
-//                        serverUrl = it.network,
-//                        channel = it.channel,
-//                        bot = it.bot,
-//                        fileRefId = it.packnum,
-//                        fileName = it.fname,
-//                        fileSize = it.fsize,
-//                    )
-//                }
-//            }
-//
-//        }
-//
-//    }
-//
-//    override fun toTO(): SearchEngineTO {
-//        return SearchEngineTO(name)
-//    }
-//
-//}
-//
+package de.maggiwuerze.xdccwebloader.service.search.skullxdcc
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import de.maggiwuerze.xdccwebloader.model.search.SearchResult
+import de.maggiwuerze.xdccwebloader.model.search.SearchResultItem
+import de.maggiwuerze.xdccwebloader.service.search.base.SearchClient
+import de.maggiwuerze.xdccwebloader.service.search.base.SearchEngineTO
+import de.maggiwuerze.xdccwebloader.service.search.base.SearchProvider
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Component
+
+@Component
+class SkullXDCCSearchProvider(
+    override val searchClient: SearchClient,
+    override val name: String = "Skull XDCC"
+) : SearchProvider {
+
+    var url: String = "https://skullxdcc.com/ws.php?sterm=%s&"
+
+    override fun search(searchTerm: String, pageable: Pageable): SearchResult {
+
+        searchClient.searchRaw(
+            baseUrlTemplate = url,
+            searchTerm = searchTerm,
+            limitResults = pageable.pageSize,
+            page = pageable.pageNumber
+        ).let { result ->
+
+            jacksonObjectMapper().readValue(result, Model.SearchResponse::class.java).let { result ->
+                val items =  result.data.map {
+                    SearchResultItem(
+                        server = it.network,
+                        serverUrl = it.network,
+                        channel = it.channel,
+                        bot = it.bot,
+                        fileRefId = it.packnum,
+                        fileName = it.fname,
+                        fileSize = it.fsize,
+                    )
+                }
+
+                return getPaginatedResult(items, pageable)
+
+            }
+
+        }
+
+    }
+
+    override fun toTO(): SearchEngineTO {
+        return SearchEngineTO(name)
+    }
+
+}
+
